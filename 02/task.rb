4373e83d44e55e030b3897831473bf64af706639
Kristjan Sever <ks4681@student.uni-lj.si>
2022-12-02 10:14:20 +0100

    add an explanation to day2

--- 02/task.rb
+++ 02/task.rb
@@ -1,7 +1,16 @@
+# I will be abusing the "cyclic property of permutations" to get the desired outcome
+# Example: Rock is 1, Paper is 2, Scissors are 3. If my opponent chooses Paper and I want
+#          to win a match, i simply add +1 to my opponents choice. If i overflow the choices
+#          (Scissors + 1 = 4), simply wrap around to 1. So if winning is +1, then losing is -1
+#          and draws are +0.
+
+# Code would be more readable if working with simple if else statemets
+# for each outcome, but I like this solution so I will leave it
+
 input = File.open("input.txt").readlines
 
-@points = {"A": 1, "B": 2, "C": 3,
-          "X": 1, "Y": 2, "Z": 3}
+@points = { "A": 1, "B": 2, "C": 3,
+            "X": 1, "Y": 2, "Z": 3 }
 ORD_DIF = "X".ord - "A".ord
 task1_score = 0
 task2_score = 0
