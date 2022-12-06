bb081ac346da3f940d9832a3251d948ad06325f7
Kristjan <56491774+KristjanSever@users.noreply.github.com>
2022-12-06 11:06:08 +0100

    Day6 (#4)
    * remove magic from day6
    
    * reformat to be consistant with variable naming

--- 06/task.rb
+++ 06/task.rb
@@ -1,20 +1,28 @@
 require 'set'
 input = File.open("input.txt").read
 
+PACKET_LENGTH_TASK1 = 4
+PACKET_LENGTH_TASK2 = 14
+
 buffer_task1 = []
 buffer_task2 = []
-res_task1 = -1
-res_task2 = -1
+res_task1 = nil
+res_task2 = nil
 input.each_char.with_index do |char, index|
   # task 1
   buffer_task1.append(char)
-  buffer_task1.shift if buffer_task1.size > 4
-  res_task1 = index + 1 if buffer_task1.size == 4 && res_task1.negative? && buffer_task1.size == buffer_task1.uniq.size
+  buffer_task1.shift if buffer_task1.size > PACKET_LENGTH_TASK1
+  if res_task1.nil? && buffer_task1.size == PACKET_LENGTH_TASK1 && buffer_task1.uniq.size == PACKET_LENGTH_TASK1
+    res_task1 = index
+  end
   # task 2
   buffer_task2.append(char)
-  buffer_task2.shift if buffer_task2.size > 14
-  res_task2 = index + 1 if buffer_task2.size == 14 && res_task2.negative? && buffer_task2.size == buffer_task2.uniq.size
-  break if res_task1.positive? && res_task2.positive?
+  buffer_task2.shift if buffer_task2.size > PACKET_LENGTH_TASK2
+  if res_task2.nil? && buffer_task2.size == PACKET_LENGTH_TASK2 && buffer_task2.uniq.size == PACKET_LENGTH_TASK2
+    res_task2 = index
+  end
+  next if res_task1.nil? || res_task2.nil?
 end
-puts res_task1
-puts res_task2
+# add 1 to result because indexes start at 0
+puts res_task1 + 1 
+puts res_task2 + 1
