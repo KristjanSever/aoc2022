3980d768e8e48edb272002ee257728058b4bde13
Kristjan Sever <ks4681@student.uni-lj.si>
2022-12-06 11:04:12 +0100

    reformat to be consistant with variable naming

--- 06/task.rb
+++ 06/task.rb
@@ -1,8 +1,8 @@
 require 'set'
 input = File.open("input.txt").read
 
-PACKET_LENGTH_TASK_1 = 4
-PACKET_LENGTH_TASK_2 = 14
+PACKET_LENGTH_TASK1 = 4
+PACKET_LENGTH_TASK2 = 14
 
 buffer_task1 = []
 buffer_task2 = []
@@ -11,14 +11,14 @@ res_task2 = nil
 input.each_char.with_index do |char, index|
   # task 1
   buffer_task1.append(char)
-  buffer_task1.shift if buffer_task1.size > PACKET_LENGTH_TASK_1
-  if res_task1.nil? && buffer_task1.size == PACKET_LENGTH_TASK_1 && buffer_task1.uniq.size == PACKET_LENGTH_TASK_1
+  buffer_task1.shift if buffer_task1.size > PACKET_LENGTH_TASK1
+  if res_task1.nil? && buffer_task1.size == PACKET_LENGTH_TASK1 && buffer_task1.uniq.size == PACKET_LENGTH_TASK1
     res_task1 = index
   end
   # task 2
   buffer_task2.append(char)
-  buffer_task2.shift if buffer_task2.size > PACKET_LENGTH_TASK_2
-  if res_task2.nil? && buffer_task2.size == PACKET_LENGTH_TASK_2 && buffer_task2.uniq.size == PACKET_LENGTH_TASK_2
+  buffer_task2.shift if buffer_task2.size > PACKET_LENGTH_TASK2
+  if res_task2.nil? && buffer_task2.size == PACKET_LENGTH_TASK2 && buffer_task2.uniq.size == PACKET_LENGTH_TASK2
     res_task2 = index
   end
   next if res_task1.nil? || res_task2.nil?
